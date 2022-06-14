222279dde97bfce5e125294fae28369669eb4712
kevelscy <kevelscy@gmail.com>
2022-06-14 06:36:14 -0400

    moar test

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import 'dotenv/config'
 import express from 'express'
+import ig from 'instagram-scraping'
+
 
 const server = express()
 
@@ -8,32 +10,73 @@ server.use(express.urlencoded({ extended: true }))
 
 server.set('PORT', process.env.PORT || 3000)
 
-server.get('/api/youtube', (req, res) => {
+server.get('/api/youtube', async (req, res) => {
   return res.status(200).json({
     data: 'youtube',
     error: null
   })
 })
 
-server.get('/api/instagram', (req, res) => {
+server.get('/api/instagram', async (req, res) => {
+  const result = await ig.scrapeTag('lcpcaracas')
+
+  // const data = result.medias.map(media => {
+  //   return {
+  //     id: media.id,
+  //     url: media.url,
+  //     likes: media.likes,
+  //     comments: media.comments,
+  //     caption: media.caption,
+  //     timestamp: media.timestamp,
+  //     image: media.image,
+  //     video: media.video,
+  //     type: media.type
+  //   }
+  // })
+
+
   return res.status(200).json({
-    data: 'instagram',
+    data: result,
     error: null
   })
 })
 
-server.get('/callback', (req, res) => {
+server.get('/callback', async (req, res) => {
+  console.log('/// here to keep track of how many times this is called')
+  console.log('Instagram code: ', req.query.code)
+
+  const data = {
+    url: url,
+    client_id : '1097733327447973',
+    client_secret : '3882292eac0c1b92fd87950ced47deb5',
+    grant_type : 'authorization_code',
+    redirect_uri : 'https://services-production-533e.up.railway.app/',
+    code : req.query.code
+  }
+
+  const url = 'https://api.instagram.com/oauth/access_token'
+
+  await req.post({
+    method: 'POST',
+    url: url,
+    body: JSON.stringify(data),
+  },
+  function (e, r, body) {
+    //body will contain the access_token
+    console.log('body', body)
+   })
+
   return res.status(200).json({
     data: true,
     error: null
   })
 })
 
-server.get('/privacy', (req, res) => {
+server.get('/privacy', async (req, res) => {
   return res.status(200).send('<div>Privacy</div>')
 })
 
-server.get('/any', (req, res) => {
+server.get('/any', async (req, res) => {
   return res.status(200).send('<div>Any</div>')
 })
 
