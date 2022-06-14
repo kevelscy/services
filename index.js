ed23ddaa6d349b2c8f5040888dd6d38611e31041
kevelscy <kevelscy@gmail.com>
2022-06-13 23:28:21 -0400

    moar routes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import express from 'express'
 
 const server = express()
@@ -8,19 +9,32 @@ server.use(express.urlencoded({ extended: true }))
 server.set('PORT', process.env.PORT || 3000)
 
 server.get('/api/youtube', (req, res) => {
-  
-  return res.json({
+  return res.status(200).json({
     data: 'youtube',
     error: null
   })
 })
 
 server.get('/api/instagram', (req, res) => {
-
-  return res.json({
+  return res.status(200).json({
     data: 'instagram',
     error: null
   })
+})
+
+server.get('/callback', (req, res) => {
+  return res.status(200).json({
+    data: true,
+    error: null
+  })
+})
+
+server.get('/privacy', (req, res) => {
+  return res.status(200).send('<div>Privacy</div>')
+})
+
+server.get('/any', (req, res) => {
+  return res.status(200).send('<div>Any</div>')
 })
 
 server.listen(server.get('PORT'), () => {
